3766dd902b801c30c56cf64b3e9475fb39b9abf3
kuloud <kuloud@outlook.com>
2025-03-30 19:34:58 +0800

    feat: add shadcn blocks

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { HeroUIProvider } from "@heroui/react";
 import { useRouter } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  return <HeroUIProvider navigate={router.push}>{children}</HeroUIProvider>;
+  return <>{children}</>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
-import { heroui } from "@heroui/react";
-
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [heroui()],
+  plugins: [],
 };
 
 export default config;
